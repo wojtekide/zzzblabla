eaaacfd3b8e38ef3998cda99ad44d9c5423be530
WojtekB <w_bielat@tlen.pl>
2017-08-20 14:54:15 +0200

    dodanie gulp-concat i gulp-browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
-gulp.task('log',function(){
-    gutil.log('Workflows are awesome');
+    gutil = require('gulp-util'),
+    kawa = require('gulp-coffee'),
+    brwzfy = require('gulp-browserify'),
+    kokokat = require('gulp-concat');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
+gulp.task('kawaz',function () {
+    gulp.src(coffeeSources)
+        .pipe(kawa({bare:true})
+            .on('error',gutil.log))
+        .pipe(gulp.dest('components/scripts'))
+});
+gulp.task('razem',function () {
+    gulp.src(jsSources)
+        .pipe(kokokat('script.js'))
+        .pipe(brwzfy())
+        .pipe(gulp.dest('builds/development/js'))
 });
